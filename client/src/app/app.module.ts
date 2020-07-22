21bea94f87e9bff122c16c168c9aacb5a6ceccc1
Garder94 <tgarder@hotmail.com>
2020-07-22 18:46:16 +0200

    Added weather, train, spotify and bikes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { BikesComponent } from './components/bikes/bikes.component';
+import { TrainComponent } from './components/train/train.component';
+import { SpotifyComponent } from './components/spotify/spotify.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    BikesComponent,
+    TrainComponent,
+    SpotifyComponent
   ],
   imports: [
     BrowserModule
