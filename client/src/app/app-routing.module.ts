2c3d97b6bc86011329d268b9d6a09663fcb0d831
Garder94 <tgarder@hotmail.com>
2021-04-05 12:48:40 +0200

    small update

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { LoginComponent } from './components/login/login.component';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+import { TrainComponent } from './components/train/train.component';
 
 const routes: Routes = [
     {path: '', component: LoginComponent, pathMatch: 'full'},
-    {path: 'admin', component: AdminPanelComponent}
+    {path: 'admin', component: AdminPanelComponent},
+    {path: 'train', component: TrainComponent}
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
